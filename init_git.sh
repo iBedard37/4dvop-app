b85d45ee32fc39240ef9d5b76957866501454092
Bedard Ianis <ianisbedard@gmail.com>
2020-03-19 11:59:53 +0100

    hoigyugyuugy

--- init_git.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-echo "# 4dvop-app" >> README.md
-git init
-git add README.md
-git commit -m "first commit"
-git remote add origin git@github.com:iBedard37/4dvop-app.git
-git push -u origin master
